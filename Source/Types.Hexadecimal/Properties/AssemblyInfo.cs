dc34d0d609ca493154c37183f0fc50b82d34be09
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-20 06:32:14 -0600

    Initial source and build commit.

---
+++ Source/Types.Hexadecimal/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Types.Hexadecimal")]
+[assembly: AssemblyDescription("Types.Hexadecimal is a library providing simple types and extensions to support the hexadecimal (hex) numeral system.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Scott Keith Thomas, Jr.")]
+[assembly: AssemblyProduct("Types.Hexadecimal")]
+[assembly: AssemblyCopyright("Copyright © 2016 Scott Keith Thomas, Jr.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("479180a8-2ccf-476c-a487-c375bf5c8668")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
